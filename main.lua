941b4bc9becb1bc84343694faf44b54f9bb9c3d3
Paweł Błach <pawel@enlighten.pl>
2015-01-15 22:03:15 +0100

    Dodano menu - v 0.2a

--- main.lua
+++ main.lua
@@ -2,20 +2,48 @@
 -- COMMON --
 -- -- -- -- -- --
 
+-- game settings --
 local gameMode
-
 local delta
+
+-- resolution --
+local screenX
+local screenY
+local offsetX = 320
+local offsetY = 150
+local areaX = 800
+local areaY = 600
 
 -- buttons --
 local isEscPressed
 local isSpacePressed
+local isEnterPressed
 local isUpPressed
 local isDownPressed
 local isLeftPressed
 local isRightPressed
 
+-- fonts --
+local fontBig
+local fontMedium
+local fontSmall
+
+-- images --
+
 function loadStuff()
-	--
+	fontBig = love.graphics.newFont(48)
+	fontMedium = love.graphics.newFont(36)
+	fontSmall = love.graphics.newFont(14)
+	resetMenu()
+	love.keyboard.setKeyRepeat(true)
+end
+
+function white()
+	love.graphics.setColor(255, 255, 255, 255)
+end
+
+function red()
+	love.graphics.setColor(255, 0, 0, 255)
 end
 
 
@@ -35,27 +63,178 @@ local menuBgG = 0
 local menuBgB = 0
 
 local menuMode
+local menuActive
+
+local menuTimer
+local menuEnabled
+local menuPositions = 4
+
+function resetMenu()
+	menuActive = 1
+	menuTimer = 0
+	menuEnabled = false
+end
 
 -- DRAW FUNCTIONS --
 
+function drawArea()
+	love.graphics.setColor(255, 0, 0, 255)
+	love.graphics.rectangle("line", offsetX, offsetY, areaX, areaY)
+end
+
 function drawMenu()
-	love.graphics.setBackgroundColor( menuBgR, menuBgG, menuBgB)
-	love.graphics.setColor( 255, 255, 255, 255 )
-	love.graphics.print("Pacman 0.1a", 700, 450)
-	love.graphics.print("Press space to continue", 700, 465)
+	if menuMode == "main" then
+		drawMainMenu()
+	end
+	if menuMode == "settings" then
+		drawSettings()
+	end
+	if menuMode == "credits" then
+		drawCredits()
+	end
+end
+
+function drawMainMenu()
+	drawArea()
+	love.graphics.setBackgroundColor(menuBgR, menuBgG, menuBgB)
+	love.graphics.setColor(255, 255, 255, 255)
+	love.graphics.setFont(fontBig)
+	love.graphics.print("PACMAN 0.1 alpha", offsetX+160, offsetY+100)
+	love.graphics.setFont(fontMedium)
+	if menuActive == 1 then red() else white() end
+	love.graphics.print("NEW GAME", offsetX+200, offsetY+300)
+	if menuActive == 2 then red() else white() end
+	love.graphics.print("GAME SETTINGS", offsetX+200, offsetY+350)
+	if menuActive == 3 then red() else white() end
+	love.graphics.print("CREDITS", offsetX+200, offsetY+400)
+	if menuActive == 4 then red() else white() end
+	love.graphics.print("QUIT", offsetX+200, offsetY+450)
+	--love.graphics.print("Press space to continue", 700, 465)
+end
+
+function drawSettings()
+	drawArea()
+	love.graphics.setBackgroundColor(menuBgR, menuBgG, menuBgB)
+	love.graphics.setColor(255, 255, 255, 255)
+	love.graphics.setFont(fontBig)
+	love.graphics.print("PACMAN 0.2 alpha", offsetX+160, offsetY+100)
+	love.graphics.setFont(fontMedium)
+	white()
+	love.graphics.print("SETTING 1", offsetX+200, offsetY+300)
+	love.graphics.print("SETTING 2", offsetX+200, offsetY+350)
+	love.graphics.print("SETTING 3", offsetX+200, offsetY+400)
+	if menuActive == 1 then red() else white() end
+	love.graphics.print("VALUE", offsetX+450, offsetY+300)
+	if menuActive == 2 then red() else white() end
+	love.graphics.print("VALUE", offsetX+450, offsetY+350)
+	if menuActive == 3 then red() else white() end
+	love.graphics.print("VALUE", offsetX+450, offsetY+400)
+	if menuActive == 4 then red() else white() end
+	love.graphics.print("SAVE", offsetX+450, offsetY+450)
+end
+
+function drawCredits()
+	drawArea()
+	love.graphics.setBackgroundColor(menuBgR, menuBgG, menuBgB)
+	love.graphics.setColor(255, 255, 255, 255)
+	love.graphics.setFont(fontBig)
+	love.graphics.print("PACMAN 0.1 alpha", offsetX+160, offsetY+100)
+	love.graphics.setFont(fontMedium)
+	love.graphics.print("CREDITS", offsetX+200, offsetY+300)
+	love.graphics.print("Game by Pawel Blach", offsetX+200, offsetY+400)
+	love.graphics.print("Press space", offsetX+200, offsetY+500)
 end
 
 -- UPDATE FUNCTIONS --
 
-function updateMenu()
-	if isSpacePressed then
-		gameMode = "game"
+function menuTimerWait()
+	if menuTimer > 0.2 then
+		menuEnabled = true
+	else
+		menuEnabled = false
 	end
 end
 
+function menuTimerAdd()
+	menuTimer = menuTimer + delta
+end
 
+function updateMenu()
+	menuTimerWait()
+	if isEscPressed then
+		if menuEnabled then
+			love.event.push( 'quit' )
+		end
+	end
+	if menuMode == "main" then
+		updateMainMenu()
+	end
+	if menuMode == "settings" then
+		updateSettings()
+	end
+	if menuMode == "credits" then
+		updateCredits()
+	end
+	if isUpPressed and (menuActive > 1) then
+		if menuEnabled then
+			menuActive = menuActive - 1
+			menuTimer = 0
+		end
+	end
+	if isDownPressed and (menuActive < menuPositions) then
+		if menuEnabled then
+			menuActive = menuActive + 1
+			menuTimer = 0
+		end
+	end
+	menuTimerAdd()
+end
 
+function updateMainMenu()
+	if isEnterPressed and menuEnabled then
+		if menuActive == 1 then
+			gameMode = "game"
+		end
+		if menuActive == 2 then
+			menuMode = "settings"
+			menuPositions = 4
+			resetMenu()
+		end
+		if menuActive == 3 then
+			menuMode = "credits"
+		end
+		if menuActive == 4 then
+			love.event.push( 'quit' )
+		end
+	end
+end
 
+function updateSettings()
+	if isEnterPressed then
+		if menuActive == 1 then
+			--
+		end
+		if menuActive == 2 then
+			--
+		end
+		if menuActive == 3 then
+			--
+		end
+		if menuActive == 4 then
+			menuPositions = 4
+			resetMenu()
+			menuMode = "main"
+		end
+	end
+end
+
+function updateCredits()
+	if isSpacePressed then
+		resetMenu()
+		menuPositions = 4
+		menuMode = "main"
+	end
+end
 
 
 
@@ -66,6 +245,7 @@ end
 -- DRAW FUNCTIONS --
 
 function drawGameDebugInfo()
+	love.graphics.setFont(fontSmall)
 	love.graphics.setColor( 255, 255, 255, 255 )
    	love.graphics.print("FPS: "..tostring(love.timer.getFPS( )), 10, 25)
 end
@@ -123,8 +303,11 @@ function love.keypressed( key, unicode )
 	if key == " " then
 		isSpacePressed = true
 	end
+	if key == "return" then
+		isEnterPressed = true
+	end
 	if key == "up" then
-		isDownPressed = true
+		isUpPressed = true
 	end
 	if key == "down" then
 		isDownPressed = true
@@ -144,8 +327,11 @@ function love.keyreleased( key, unicode )
 	if key == " " then
 		isSpacePressed = false
 	end
+	if key == "return" then
+		isEnterPressed = false
+	end
 	if key == "up" then
-		isDownPressed = false
+		isUpPressed = false
 	end
 	if key == "down" then
 		isDownPressed = false
